510f8c16dc2537bb3bd63f3cca70966f84176d6b
PythoSalaf <alfaheed1010@gmail.com>
2026-09-16 15:27:00 +0100

    add Inngest integration for Clerk user sync and deletion

---
+++ TechCartBackend/src/config/inngest.js
@@ -0,0 +1,35 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+export const inngest = new Inngest({ id: "techcart-app" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user", triggers: { event: "clerk/user.created" } },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_address, first_name, last_name, image_url } = event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_address[0]?.email_address,
+      name: `${first_name || ""}  ${last_name || ""}` || "User",
+      imageURL: image_url,
+      addresses: [],
+      wishList: [],
+    };
+
+    await User.create(newUser);
+  },
+);
+
+const deleteUserFromDatabase = inngest.createFunction(
+  { id: "delete-user-from-db", triggers: { event: "clerk/user.deleted" } },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  },
+);
+
+export const functions = [syncUser, deleteUserFromDatabase];
--- TechCartBackend/src/server.js
+++ TechCartBackend/src/server.js
@@ -3,10 +3,16 @@ import { clerkMiddleware } from "@clerk/express";
 import path from "path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
+
 const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(clerkMiddleware()); // adds auth objects under the request  (req.auth, req.session, req.user, etc.)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Sucess" });
 });
